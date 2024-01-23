72790e3686ed750947855ec4ad7842396143f31c
Djioleu Florian <nsiegningflorian@gmail.com>
2024-01-23 10:26:41 +0100

    Add the Vocabulary base model

---
+++ src/main/java/org/weller/vocab/model/Vocabulary.java
@@ -0,0 +1,12 @@
+package org.weller.vocab.model;
+
+import java.util.UUID;
+
+public class Vocabulary {
+    private UUID id;
+    private BookCase bookCase;
+    private String name;
+    private String description;
+    private String wordsOriginalLanguage;
+    private String wordMeaningLanguage;
+}
