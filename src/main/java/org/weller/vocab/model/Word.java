4ceb2d183136bac7d704d004b05fefca2043bc69
Djioleu Florian <nsiegningflorian@gmail.com>
2024-01-23 10:26:04 +0100

    Add a Word base model

---
+++ src/main/java/org/weller/vocab/model/Word.java
@@ -0,0 +1,10 @@
+package org.weller.vocab.model;
+
+import java.util.UUID;
+
+public class Word {
+    private UUID id;
+    private String originalWord;
+    private String wordTranslation;
+    private String pronunciation;
+}
